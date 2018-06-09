9ebd99842cd846135948daeca6b0092704f35770
jan-patrick <jan.schneider@hfg-gmuend.de>
2018-06-09 23:20:09 +0200

    maybe this is working, but it is using too much dynamic storage
    maybe we will be using two arduinos

--- sense-chess/sense-chess.ino
+++ sense-chess/sense-chess.ino
@@ -5,6 +5,12 @@
 
 String field = "d7";
 char val;
+int myArray[] = {};
+String ledArray[] = { "a1 1","a2 2"};
+const int ledfield = 2;
+const int ledpin = 1;
+int pinMatrix[64][64] = { {"a1",1}, {"a2",2}, {"a3",3}, {"a4",4}, {"a5",5}, {"a6",6}, {"a7",7}, {"a8",8} 
+};
 
 void setup(void) 
 {
@@ -22,15 +28,58 @@ void loop(void)
 
     if(val == "connected") {
         digitalWrite(LED_BUILTIN, HIGH);
-    }
-    else if(rec.indexOf("d5")) {
-        digitalWrite(LED_BUILTIN, HIGH);
-    }else if(val == "d6") {
-        digitalWrite(LED_BUILTIN, LOW);
-    }
-    else{ 
-        digitalWrite(LED_BUILTIN, LOW);
-    }
+    }else{
+      // !!!!!!
+      // only works if Arduino receives only 5 fields, f.e.: d5, h7, g2, a6, f3
+      // !!!!!!
+ /*     int reclength = rec.length();
+      if(reclength>=17){
+        String stringFive.substring(16, 18);
+        String stringFour.substring(12, 14);
+        String stringThree.substring(8, 10);
+        String stringTwo.substring(4, 6);
+        String stringOne.substring(0, 2);
+      }
+      else if (reclength>=13){
+        String stringFour.substring(12, 14);
+        String stringThree.substring(8, 10);
+        String stringTwo.substring(4, 6);
+        String stringOne.substring(0, 2);
+      }
+      else if (reclength>=9){
+        String stringThree.substring(8, 10);
+        String stringTwo.substring(4, 6);
+        String stringOne.substring(0, 2);
+      }
+      else if (reclength>=5){
+        String stringTwo.substring(4, 6);
+        String stringOne.substring(0, 2);
+      }
+      else{
+        String stringOne.substring(0, 2);
+      }
+  */  memset(myArray, 0, sizeof(myArray));
+      char delimiter[] = ",;";
+      char *ptr;
+          
+      // initialisieren und ersten Abschnitt erstellen
+      ptr = strtok(val, delimiter);
+      int i = 0;
+      while(ptr != NULL) {
+        myArray[i]=ptr;
+        // naechsten Abschnitt erstellen
+        ptr = strtok(NULL, delimiter);
+        i++;
+      }
+      digitalWrite(LED_BUILTIN, LOW);
+      for(int t =0; t<=sizeof(myArray);t++){
+        for(int j =0; j<=sizeof(ledArray);j++){
+           if(pinMatrix[j][0]==myArray[t]){
+              digitalWrite(LED_BUILTIN, HIGH);
+           }
+        }
+      }  
+    }  
     delay(100);
   } 
   else {
