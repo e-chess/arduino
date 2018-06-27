17001d1e78ed530df167573d2b611127a7fe3c82
jan-patrick <jan.schneider@hfg-gmuend.de>
2018-06-27 20:53:49 +0200

    ledTest with direct color change

--- ledTest/ledTest.ino
+++ ledTest/ledTest.ino
@@ -6,11 +6,11 @@
 #include <Adafruit_NeoPixel.h>
 
 #define PIN 6
-#define NUMLEDS 5
+#define NUMLEDS 8
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMLEDS, PIN, NEO_GRB + NEO_KHZ800);
 
-const int SETLEDS = 500; // delay for half a second
+const int SETLEDS = 5000; // delay for half a second
 
 void setup() {
   pixels.begin(); // This initializes the NeoPixel library
@@ -20,19 +20,11 @@ void loop() {
   for(int i=0;i<NUMLEDS;i++){
     pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
     pixels.show(); // This sends the updated pixel color to the hardware
-    delay(SETLEDS);
   }
-  delay(5000);
+  delay(SETLEDS);
   for(int i=0;i<NUMLEDS;i++){
     pixels.setPixelColor(i, pixels.Color(255,255,255)); // Moderately bright green color.
     pixels.show(); // This sends the updated pixel color to the hardware
-    delay(SETLEDS);
   }
-  delay(5000);
-  for(int i=0;i<NUMLEDS;i++){
-    pixels.setPixelColor(i, pixels.Color(0,0,0)); // Moderately bright green color.
-    pixels.show(); // This sends the updated pixel color to the hardware
-    delay(SETLEDS);
-  }
-  delay(5000);
+  delay(SETLEDS);
 }
